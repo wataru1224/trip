42acd5e275d95585f951b426bd6b009a51927776
wataru1224 <watarue112037@yahoo.co.jp>
2021-06-06 06:33:20 +0900

    SSL修正

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -8,8 +8,6 @@
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.4/vegas.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ secure_asset('./css/home.css')}}">
-    <!-- <link rel="stylesheet" href="{{asset('./css/home.css"> -->
-    <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
     <title>Instavel</title>
 </head>
 
